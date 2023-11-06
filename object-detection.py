48fdbcc2ed1412225a85497430ce5d0b8b6d93b5
4m4n-x-b4w4ne <amanmbawane@gmail.com>
2023-11-06 19:03:37 +0530

    Enhanced Documentation

--- object-detection.py
+++ object-detection.py
@@ -1,34 +1,43 @@
 # Import necessary libraries
-import cv2
-import subprocess
-import pygame
+import cv2                # OpenCV library for computer vision
+import subprocess         # subprocess library to execute command-line processes
+import pygame             # pygame library for audio playback
 
-# Define the class names, model configuration, and model weights paths
+# Define a list to store class names
 classNames = []
+
+# Specify the path to the file containing class names (COCO dataset)
 classFile = "/home/pi/Desktop/Object_Detection_Files/coco.names"
 
 # Read class names from the specified file
 with open(classFile, "rt") as f:
     classNames = f.read().rstrip("\n").split("\n")
 
+# Define paths to the model configuration and weights files
 configPath = "/home/pi/Desktop/Object_Detection_Files/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
 weightsPath = "/home/pi/Desktop/Object_Detection_Files/frozen_inference_graph.pb"
 
-# Create an instance of the object detection model
+# Create an instance of the object detection model using OpenCV
 net = cv2.dnn_DetectionModel(weightsPath, configPath)
+
+# Set input size, scale, mean, and swap channels for object detection
 net.setInputSize(320, 320)
 net.setInputScale(1.0 / 127.5)
 net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
-# Define a function to detect objects
+# Define a function to detect objects in an image
 def getObjects(img, thres, nms, draw=True, objects=[]):
+    # Perform object detection using the specified image, threshold, and non-maximum suppression parameters
     classIds, confs, bbox = net.detect(img, confThreshold=thres, nmsThreshold=nms)
 
+    # If a specific list of objects is not provided, use all class names
     if len(objects) == 0:
         objects = classNames
 
     objectInfo = []
+
+    # Process detected objects and draw bounding boxes if 'draw' is True
     if len(classIds) != 0:
         for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
             className = classNames[classId - 1]
@@ -57,13 +66,17 @@ def getObjects(img, thres, nms, draw=True, objects=[]):
 
     return img, objectInfo
 
+# Main part of the code
 if __name__ == "__main":
-    # Initialize the video capture
+
+    # Initialize video capture from the default camera (camera index 0)
     cap = cv2.VideoCapture(0)
+
+    # Set video capture dimensions
     cap.set(3, 640)
     cap.set(4, 480)
 
-    # Notify the start of object detection and Using Festival Text is converted into Speech
+    # Notify the start of object detection
     text_to_speak = "Starting the Object Detection device, will start detecting objects in your surroundings soon"
     festival_output = subprocess.check_output(["festival", "--tts"], input=text_to_speak.encode())
     pygame.mixer.init()
@@ -72,9 +85,16 @@ if __name__ == "__main":
     pygame.time.wait(int(audio.get_length() * 1000))
 
     while True:
+        # Read a frame from the video feed
         success, img = cap.read()
+
+        # Perform object detection with specified threshold and non-maximum suppression parameters
         result, objectInfo = getObjects(img, 0.45, 0.2)
+
+        # Print detected object information
         print(objectInfo)
+
+        # If objects are detected, provide audio feedback for each object
         if len(objectInfo) > 0:
             for i in objectInfo:
                 print(i[-1])
@@ -84,4 +104,6 @@ if __name__ == "__main":
                 audio = pygame.mixer.Sound(buffer=festival_output)
                 audio.play()
                 pygame.time.wait(int(audio.get_length() * 1000))
+
+        # Wait for a key press (1 millisecond) and continue processing the next frame
         cv2.waitKey(1)
